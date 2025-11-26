cc1e495e72c687ea1187dd7863616495870a14a1
Zhambyl Alikhan <alykhanzhambyl@icloud.com>
2025-11-26 18:13:17 +0500

    tuple of hashes and keys

--- tasks/task_2/task2.py
+++ tasks/task_2/task2.py
@@ -2,25 +2,30 @@ from pathlib import Path
 import hashlib
 
 dir = Path("input")
-h = dir / "file_00.data"
-h = hashlib.sha3_256(h.read_bytes()).hexdigest()
-print(f'hash = {h}')
-k = 1
-for i in h:
-    k *= (int(i, 16) + 1)
-    
-print(f'sorting key = {k}')
+hashes = []
 
-# for f in dir.iterdir():
-#     h = hashlib.sha3_256(f.read_bytes()).hexdigest()
-#     key = []
-#     for i in h:
-#         i = int(i, base=16)
-#         i += 1
-# d = int(i, base=16)
-#     x = d + 1
-#     k = k * x 
+for f in dir.iterdir():
+    h = hashlib.sha3_256(f.read_bytes()).hexdigest()
+    k = 1
+    for i in h:
+        k *= (int(i, 16) + 1)
+    hashes.append((k,h))
     
+
+for i in hashes:
+    print(i)
+    
+
+# h = dir / "file_00.data"
+# h = hashlib.sha3_256(h.read_bytes()).hexdigest()
+# print(f'hash = {h}')
+# k = 1
+# for i in h:
+#     k *= (int(i, 16) + 1)
+    
+# print(f'sorting key = {k}')
 # for h in dir.iterdir():
 #     h = hashlib.sha3_256(h.read_bytes()).hexdigest()
 #     print(h)
+# print(f'hash = {h}')
+    # print(f'sorting key = {k}')
